b6f5bd619617d9e30f38b04260fa34c7a44aa675
Cameron Gallichan <camerongallichan@gmail.com>
2024-06-19 11:37:43 +0100

    reformatting

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -31,7 +31,8 @@ public static class IdentityServiceExtensions
                 options.TokenValidationParameters = new TokenValidationParameters
                 {
                     ValidateIssuerSigningKey = true,
-                    IssuerSigningKey  = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Token:Key"])),
+                    IssuerSigningKey  = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(config["Token:Key"])),
                     ValidIssuer = config["Token:Issuer"],
                     ValidateIssuer = true,
                     ValidateAudience = false
